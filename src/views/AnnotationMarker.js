8a25cecb5255ad2feacdeb9dbb5069b7ee45073a
Tuomas Hatakka <tuomas.hatakka@gmail.com>
2018-03-24 14:01:34 +0200

    Add option to toggle the alignment for the overlay

--- src/views/AnnotationMarker.js
+++ src/views/AnnotationMarker.js
@@ -1,8 +1,8 @@
 'use babel'
 
+import self from 'autobind-decorator'
 import { TextEditor, CompositeDisposable } from 'atom'
 import { getAnnotatedRanges, getHead, editorIsVisible } from '../utils'
-
 
 function destroyInactive (item) {
   if (item != this.editor || editorIsVisible(this.editor))
@@ -11,10 +11,27 @@ function destroyInactive (item) {
 }
 
 
-function getOverlayTemplate ({ severity, excerpt }) {
+function getOverlayTemplate ({ severity, excerpt, ...rest }) {
+  console.log("Message rest:", rest)
   return `
-    <h4 class='severity'>${severity}</h4>
-    <article class='excerpt'>${excerpt}</article>`
+    <header>
+      <h4 class='severity'>${severity}</h4>
+      <nav class='menu'>
+
+        <span class='flip icon icon-jump-up'></span>
+
+        <span class='hidden'>
+          <span class='toggle icon icon-settings'></span>
+          <ul class='list-group'>
+            <li class='list-item'>Disable for ${severity}</li>
+            <li class='list-item'>Disable for this entry</li>
+          </ul>
+        </span>
+
+      </nav>
+    </header>
+    <article class='excerpt'>${excerpt}</article>
+  `
 }
 
 
@@ -26,6 +43,8 @@ export default class AnnotationMarker {
     class: 'lint-annotation-overlay',
     type:  'overlay',
   }
+
+  orientation = 'below'
 
   constructor (textEditor) {
 
@@ -43,6 +62,7 @@ export default class AnnotationMarker {
     this.editor = textEditor
     this.subscriptions = new CompositeDisposable()
     this.subscriptions.add(...subscriptions)
+
   }
 
   didChangeSelection (selection) {
@@ -68,12 +88,30 @@ export default class AnnotationMarker {
     this.editor.decorateMarker(this.marker, decals)
   }
 
+  applyOrientationClass () {
+    let el = this.item.parentElement
+    if (el) {
+      el.classList.remove('above', 'below')
+      el.classList.add(this.orientation)
+    }
+  }
+
+  @self
+  changeOrientation () {
+    this.orientation = this.orientation === 'below' ? 'above' : 'below'
+    this.applyOrientationClass()
+  }
+
   show (message) {
     if (!message)
       return
     this.decorateEditor(this.editor)
     this.item.setAttribute('class', 'message ' + message.severity)
     this.item.innerHTML = AnnotationMarker.getContent(message)
+    this.applyOrientationClass()
+
+    let flipButton = this.item.querySelector('.flip')
+    flipButton.addEventListener('click', this.changeOrientation)
   }
 
   hide () {
